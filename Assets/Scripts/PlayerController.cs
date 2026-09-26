ebee966ab17a0ebe8ef8d882194e3819a6562bf9
Tonoy <tonoychan55@gmail.com>
2026-09-26 06:26:54 +0530

    Objects PickUp and Thrown

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,12 +7,19 @@ public class PlayerController : MonoBehaviour
     public InputActionReference moveAction;
     public InputActionReference jumpAction;
     public InputActionReference lookAction;
+    [Tooltip("Attack Action")]
+    public InputActionReference LeftMouseAction;
+    public Transform holdPoint;
     
     public CharacterController controller;
+    
+    public LayerMask WhatIsStock;
     
     [Header("Private References")]
     [SerializeField]
     private Camera _camera;
+    [SerializeField]
+    private GameObject heldPickUpObject;
     
     [Header("Value Factor Controls")]
     [SerializeField]
@@ -25,6 +32,10 @@ public class PlayerController : MonoBehaviour
     private float verticalAngle;
     [SerializeField]
     private float gravSpeedMultiplier;
+    [SerializeField]
+    private float interactionRange;
+    [SerializeField]
+    private float throwForce;
     
     [Header("Value For Debug")]
     [SerializeField]
@@ -33,6 +44,7 @@ public class PlayerController : MonoBehaviour
     private float horizontalRotation;
     [SerializeField]
     private float verticalRotation;
+
     
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -78,5 +90,35 @@ public class PlayerController : MonoBehaviour
         //Final Move Amount
         controller.Move(moveAmount);
         
+        //Checking For PickUp
+        if (!heldPickUpObject)
+        {
+            if (Mouse.current.leftButton.wasPressedThisFrame)
+            {
+                Ray ray = _camera.ViewportPointToRay(new Vector3(0.5f,0.5f,0f));
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit, interactionRange, WhatIsStock))
+                {
+                    heldPickUpObject = hit.transform.gameObject;
+                    heldPickUpObject.transform.SetParent(holdPoint);
+                    heldPickUpObject.transform.localPosition = new Vector3(0.2f,0f,0f);
+                    heldPickUpObject.transform.localRotation = Quaternion.identity;
+                    heldPickUpObject.GetComponent<Rigidbody>().isKinematic = true;
+                }
+            }
+        }
+        else
+        {
+            //Leaving or throwing Object
+            if (Mouse.current.rightButton.wasPressedThisFrame)
+            {
+                var heldObjectRigidBody = heldPickUpObject.GetComponent<Rigidbody>();
+                heldPickUpObject.transform.SetParent(null);
+                heldPickUpObject = null;
+                heldObjectRigidBody.isKinematic = false;
+                heldObjectRigidBody.AddForce(_camera.transform.forward * throwForce, ForceMode.Impulse);
+            }
+        }
     }
 }
